22ae58e60ebc1088222fc55708da00fe21aebeda
Julian Casaburi <juliancasaburi@gmail.com>
2022-06-19 23:07:50 -0300

    Refactorizar jobs (extract class ISeedingService & extract method)

--- bbdd2PromocionApp/src/main/java/com/bbdd2promocion/api/APIController.java
+++ bbdd2PromocionApp/src/main/java/com/bbdd2promocion/api/APIController.java
@@ -6,34 +6,19 @@ import javax.inject.Inject;
 
 import com.bbdd2promocion.model.Accident;
 import com.bbdd2promocion.model.TestModel;
-import com.bbdd2promocion.seed.mongodb.InsertionJobConfiguration;
-import com.bbdd2promocion.seed.mongodb.MongoDBConfiguration;
-import com.bbdd2promocion.seed.mongodb.TestModelInsertionJobConfiguration;
-import com.bbdd2promocion.seed.postgresql.TestModelPostgresInsertionJobConfiguration;
-import com.bbdd2promocion.seed.postgresql.HibernateConfiguration;
 import com.bbdd2promocion.service.IAccidentService;
-import org.springframework.batch.core.JobParametersBuilder;
+import com.bbdd2promocion.service.ISeedingService;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Metrics;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -59,6 +44,12 @@ public class APIController {
 	public IAccidentService accidentService;
 
 	/**
+	 * Es el servicio relacionado con los seeding jobs.
+	 */
+	@Inject
+	public ISeedingService seedingService;
+
+	/**
 	 * Getter.
 	 *
 	 * @return el servicio relacionado con los TestModel.
@@ -76,49 +67,30 @@ public class APIController {
 		return this.accidentService;
 	}
 
+	/**
+	 * Getter.
+	 *
+	 * @return el servicio relacionado con los seeding jobs.
+	 */
+	private ISeedingService getSeedingService() {
+		return this.seedingService;
+	}
+
 	@PostMapping("/seed/mongodb/testModel")
-	public ResponseEntity seedTestModelMongoDB() throws JobExecutionAlreadyRunningException, JobRestartException,
-			JobInstanceAlreadyCompleteException, JobParametersInvalidException
-	{
-		Class<?>[] configurationClasses = {TestModelInsertionJobConfiguration.class, MongoDBConfiguration.class};
-		ApplicationContext context = new AnnotationConfigApplicationContext(configurationClasses);
-		Job testModelinsertionJob = context.getBean("testModelInsertionJob", Job.class);
-		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-		JobParameters params = new JobParametersBuilder()
-				.addString("JobID", String.valueOf(System.currentTimeMillis()))
-				.toJobParameters();
-		jobLauncher.run(testModelinsertionJob, params);
+	public ResponseEntity seedTestModelMongoDB() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+		this.getSeedingService().seedTestModelMongoDB();
 		return ResponseEntity.status(HttpStatus.OK).body("mongodb testModel seeding job started");
 	}
 
 	@PostMapping("/seed/mongodb/accident")
-	public ResponseEntity seedAccidentMongoDB() throws JobExecutionAlreadyRunningException, JobRestartException,
-			JobInstanceAlreadyCompleteException, JobParametersInvalidException
-	{
-		Class<?>[] configurationClasses = { InsertionJobConfiguration.class, MongoDBConfiguration.class };
-		ApplicationContext context = new AnnotationConfigApplicationContext(configurationClasses);
-		Job insertionJob = context.getBean("insertionJob", Job.class);
-		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-		JobParameters params = new JobParametersBuilder()
-				.addString("JobID", String.valueOf(System.currentTimeMillis()))
-				.toJobParameters();
-		jobLauncher.run(insertionJob, params);
-		return ResponseEntity.status(HttpStatus.OK)
-				.body("mongodb accident seeding job started");
+	public ResponseEntity seedAccidentMongoDB() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+		this.getSeedingService().seedAccidentMongoDB();
+		return ResponseEntity.status(HttpStatus.OK).body("mongodb accident seeding job started");
 	}
 
 	@PostMapping("/seed/postgresql/testModel")
-	public ResponseEntity seedTestModelPostgrSQL() throws JobExecutionAlreadyRunningException, JobRestartException,
-			JobInstanceAlreadyCompleteException, JobParametersInvalidException
-	{
-		Class<?>[] configurationClasses = { TestModelPostgresInsertionJobConfiguration.class, HibernateConfiguration.class};
-		ApplicationContext context = new AnnotationConfigApplicationContext(configurationClasses);
-		Job testModelinsertionJob = context.getBean("testModelPostgresInsertionJob", Job.class);
-		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-		JobParameters params = new JobParametersBuilder()
-				.addString("JobID", String.valueOf(System.currentTimeMillis()))
-				.toJobParameters();
-		jobLauncher.run(testModelinsertionJob, params);
+	public ResponseEntity seedTestModelPostgreSQL() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+		this.getSeedingService().seedTestModelPostgreSQL();
 		return ResponseEntity.status(HttpStatus.OK).body("postgresql testModel seeding job started");
 	}
 
