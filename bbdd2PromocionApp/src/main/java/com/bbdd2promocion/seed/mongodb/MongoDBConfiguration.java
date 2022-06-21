017ef730687c35827d06f6adde244b3ecbb009e0
Julian Casaburi <juliancasaburi@gmail.com>
2022-06-21 00:39:22 -0300

    Configurar creación automática de índices MongoDB (el Model debe tener la annotation @Document)

--- bbdd2PromocionApp/src/main/java/com/bbdd2promocion/seed/mongodb/MongoDBConfiguration.java
+++ bbdd2PromocionApp/src/main/java/com/bbdd2promocion/seed/mongodb/MongoDBConfiguration.java
@@ -6,10 +6,11 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-public class MongoDBConfiguration {
+public class MongoDBConfiguration extends AbstractMongoClientConfiguration {
 
     @Value("${spring.data.mongodb.host:mongodb}")
     private String mongodbHost;
@@ -20,11 +21,27 @@ public class MongoDBConfiguration {
     @Value("${spring.data.mongodb.database:bbdd2_promocion}")
     private String mongodbDatabase;
 
+    @Override
+    protected String getDatabaseName() {
+        return mongodbDatabase;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        String connectionString = "mongodb://" + this.mongodbHost + ":" + Integer.parseInt(this.mongodbPort) + "/" + this.getDatabaseName();
+        MongoClient mClient = MongoClients.create(connectionString);
+        return mClient;
+
+    }
+
+    @Override
+    public boolean autoIndexCreation() {
+        return true;
+    }
+
     @Bean
     public MongoTemplate mongoTemplate() {
-        String connectionString = "mongodb://" + this.mongodbHost + ":" + Integer.parseInt(this.mongodbPort) + "/" + this.mongodbDatabase;
-        MongoClient mongoClient = MongoClients.create(connectionString);
-        return new MongoTemplate(mongoClient, this.mongodbDatabase);
+        return new MongoTemplate(this.mongoClient(), this.getDatabaseName());
     }
 
 }
